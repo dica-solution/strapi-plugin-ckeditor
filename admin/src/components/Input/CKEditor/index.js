894c1234afa16bfbb60f86cde490c72d09203aca
iamtun <letuan19431791.iuh@gmail.com>
2024-02-28 14:27:15 +0700

    feat: replace html encode & inside formula

--- admin/src/components/Input/CKEditor/index.js
+++ admin/src/components/Input/CKEditor/index.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, {useEffect, useRef, useState} from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import { CKEditor } from "@ckeditor/ckeditor5-react";
-import { Box, Loader } from '@strapi/design-system';
+import {CKEditor} from "@ckeditor/ckeditor5-react";
+import {Box, Loader} from "@strapi/design-system";
 
 import {getConfiguration} from "./configuration";
 import {getGlobalStyling} from "./styling";
@@ -13,29 +13,34 @@ import ckeditor5EditorClassicDll from "@ckeditor/ckeditor5-editor-classic/build/
 
 const GlobalStyling = getGlobalStyling();
 
-const Wrapper = styled("div")`${({ editorStyles }) => editorStyles}`;
+const Wrapper = styled("div")`
+  ${({editorStyles}) => editorStyles}
+`;
 
-const Editor = ({ onChange, name, value, disabled, preset, maxLength }) => {
+const Editor = ({onChange, name, value, disabled, preset, maxLength}) => {
+  const [editorInstance, setEditorInstance] = useState(false);
 
-  const [ editorInstance, setEditorInstance ] = useState(false);
-  
   const [mediaLibVisible, setMediaLibVisible] = useState(false);
-  
+
   const [uploadPluginConfig, setUploadPluginConfig] = useState(null);
-  
+
   const [config, setConfig] = useState(null);
 
   const [lengthMax, setLengthMax] = useState(false);
 
   const wordCounter = useRef(null);
-  
-  const handleToggleMediaLib = () => setMediaLibVisible(prev => !prev);
 
-  const handleCounter = (number) => number > maxLength ? setLengthMax(true) : setLengthMax(false);
-  
+  const handleToggleMediaLib = () => setMediaLibVisible((prev) => !prev);
+
+  const handleCounter = (number) =>
+    number > maxLength ? setLengthMax(true) : setLengthMax(false);
+
   useEffect(() => {
     (async () => {
-      const {currentConfig, uploadPluginConfig} = await getConfiguration(preset, handleToggleMediaLib);
+      const {currentConfig, uploadPluginConfig} = await getConfiguration(
+        preset,
+        handleToggleMediaLib
+      );
       setConfig(currentConfig);
       setUploadPluginConfig(uploadPluginConfig);
     })();
@@ -43,49 +48,77 @@ const Editor = ({ onChange, name, value, disabled, preset, maxLength }) => {
 
   return (
     <>
-    {config && <GlobalStyling />}
-    <Wrapper editorStyles={config?.styles} >
-      {!config &&
-      <LoaderBox hasRadius background="neutral100">
-        <Loader>Loading...</Loader>
-      </LoaderBox>}
-      {config &&
+      {config && <GlobalStyling />}
+      <Wrapper editorStyles={config?.styles}>
+        {!config && (
+          <LoaderBox hasRadius background="neutral100">
+            <Loader>Loading...</Loader>
+          </LoaderBox>
+        )}
+        {config && (
           <CKEditor
             editor={window.CKEditor5.editorClassic.ClassicEditor}
             config={config?.editorConfig}
             disabled={disabled}
             data={value}
             onReady={(editor) => {
-              
-              if(config.editorConfig.WordCountPlugin){
-                const wordCountPlugin = editor.plugins.get( 'WordCount' );
-                wordCountPlugin.on( 'update', ( evt, stats ) =>handleCounter(stats.characters));
+              if (config.editorConfig.WordCountPlugin) {
+                const wordCountPlugin = editor.plugins.get("WordCount");
+                wordCountPlugin.on("update", (evt, stats) =>
+                  handleCounter(stats.characters)
+                );
                 const wordCountWrapper = wordCounter.current;
-                wordCountWrapper?.appendChild( wordCountPlugin.wordCountContainer );
+                wordCountWrapper?.appendChild(
+                  wordCountPlugin.wordCountContainer
+                );
               }
 
-              if(editor.plugins.has( 'ImageUploadEditing' )){
-                editor.plugins.get( 'ImageUploadEditing' ).on( 'uploadComplete', ( evt, { data, imageElement } ) =>    
-                  editor.model.change( writer => writer.setAttribute( 'alt', data.alt, imageElement ) ) ); 
+              if (editor.plugins.has("ImageUploadEditing")) {
+                editor.plugins
+                  .get("ImageUploadEditing")
+                  .on("uploadComplete", (evt, {data, imageElement}) =>
+                    editor.model.change((writer) =>
+                      writer.setAttribute("alt", data.alt, imageElement)
+                    )
+                  );
               }
-            
-              setEditorInstance( editor );
+
+              setEditorInstance(editor);
             }}
             onChange={(event, editor) => {
               const data = editor.getData();
-              onChange({ target: { name, value: data } });
+              // replace html encode inside formula
+              const dataCleaned = data.replace(
+                /<span class="math-tex">(.*?)<\/span>/g,
+                function (match, p1) {
+                  return (
+                    '<span class="math-tex">' +
+                    p1.replace(/&amp;/g, "&") +
+                    "</span>"
+                  );
+                }
+              );
+              onChange({target: {name, value: dataCleaned}});
             }}
           />
-      }
-      {config && config.editorConfig.WordCountPlugin && 
-          <CounterLoaderBox 
-            color={lengthMax?"danger500":"neutral400"} 
-            ref={wordCounter}>
-              {!editorInstance && <Loader small>Loading...</Loader>}
+        )}
+        {config && config.editorConfig.WordCountPlugin && (
+          <CounterLoaderBox
+            color={lengthMax ? "danger500" : "neutral400"}
+            ref={wordCounter}
+          >
+            {!editorInstance && <Loader small>Loading...</Loader>}
           </CounterLoaderBox>
-      }
-      {uploadPluginConfig && <MediaLib isOpen={mediaLibVisible} onToggle={handleToggleMediaLib} editor={editorInstance} uploadConfig={uploadPluginConfig} />}
-    </Wrapper>
+        )}
+        {uploadPluginConfig && (
+          <MediaLib
+            isOpen={mediaLibVisible}
+            onToggle={handleToggleMediaLib}
+            editor={editorInstance}
+            uploadConfig={uploadPluginConfig}
+          />
+        )}
+      </Wrapper>
     </>
   );
 };
@@ -103,17 +136,17 @@ Editor.propTypes = {
 };
 
 const CounterLoaderBox = styled(Box)`
-  display:flex;
+  display: flex;
   width: 100%;
   justify-content: flex-end;
   align-items: center;
-  `
+`;
 const LoaderBox = styled(Box)`
-  display:flex;
+  display: flex;
   height: 200px;
   width: 100%;
   justify-content: center;
   align-items: center;
-  `
+`;
 
 export default Editor;
